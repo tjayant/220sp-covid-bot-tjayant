9f377858f88253bdfe2613efaf88b3124f72838f
Jayant.Thomas <jayant.thomas@changehealthcare.com>
2020-05-06 08:58:21 -0700

    Initial commits

---
+++ test_pset.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `covid_bot` package."""
+
+from unittest import TestCase
+
+
+class SampleTests(TestCase):
+    def test_basic(self):
+        pass
